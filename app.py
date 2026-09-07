37823f930674e6abcc80a567e44a71323c55de89
JERONIMO LIMA DE SOUZA <jeronimolimasouza@gmail.com>
2026-09-06 22:39:54 -0300

    Remove unnecessary whitespace and comments

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ potencia = float(input("Digite a potencia do aparelho em watts: " ))
 horasDia = int(input("Digite o tempo medio de uso diario em horas: " ))
 
 #Processamento das informações.
-
 Calcule = (potencia * horasDia * 0.75) / 1000
 
 # Saída das informações.
-
 print("O consumo estimado: ", Calcule, "kWH/mês")
