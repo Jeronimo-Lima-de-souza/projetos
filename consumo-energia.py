13c1244a916d0a28822869105c29683e259dfb0d
Jeronimo Lima de Souza <jeronimolimasouza@gmail.com>
2026-09-06 22:09:05 -0300

    Initial commit

---
+++ consumo-energia.py
@@ -0,0 +1,16 @@
+# consumo-energia
+
+# Entrada das informações.
+
+input("Digite o nome do aparelho: ")
+potencia = float(input("Digite a potencia do aparelho em watts: " ))
+horasDia = int(input("Digite o tempo medio de uso diario em horas: " ))
+
+#Processamento das informações.
+
+Calcule = (potencia * horasDia * 0.75) / 1000
+
+# Saída das informações.
+
+print("O consumo estimado: ", Calcule, "kWH/mês")
+    
